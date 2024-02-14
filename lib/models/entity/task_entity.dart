a5d9ba417dc3ec52425d5b82099e79208ba2af7f
candra sukma <candra.sukma@nuon.id>
2024-02-14 08:42:55 +0700

    MVC Folder Structure

---
+++ lib/models/entity/task_entity.dart
@@ -0,0 +1,5 @@
+/// created by : candra
+/// email : csukma.codes@gmail.com
+/// date : Feb 14, 2024 08:37:55
+/// project : todo
+
---
+++ lib/utils/constant.dart
@@ -0,0 +1,5 @@
+/// created by : candra
+/// email : csukma.codes@gmail.com
+/// date : Feb 14, 2024 08:41:36
+/// project : todo
+

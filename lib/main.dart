d149850f37f680265674a1a164622c69124eeb87
candra sukma <candra.sukma@nuon.id>
2024-04-23 17:06:29 +0700

    repo add provider

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:todo/controllers/task_controller.dart';
 import 'package:todo/models/entity/task_entity.dart';
+import 'package:todo/models/repositories/task_repositories.dart';
+import 'package:todo/models/repositories/task_repositories_impl.dart';
 import 'package:todo/views/task_screen.dart';
 
 Future<void> main() async {
@@ -15,18 +17,22 @@ Future<void> main() async {
 
   runApp(MultiProvider(
     providers: [
-      ChangeNotifierProvider(create: (_) => TaskController()),
+      Provider<TaskRepositories>(
+        create: (context) => TaskRepositoriesImpl(context),
+      ),
+      ChangeNotifierProvider(create: (context) => TaskController(context)),
+
     ],
-    child: const MyApp(),
+    child:  MyApp(),
   ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: TaskScreen(title: 'Todo'),
     );
   }
